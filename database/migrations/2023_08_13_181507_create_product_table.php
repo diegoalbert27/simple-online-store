38e9fb5be20ddb4eef225fa0bd04d152cf0946af
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-08-13 15:40:26 -0400

    product model added

--- database/migrations/2023_08_13_181507_create_product_table.php
+++ database/migrations/2023_08_13_181507_create_product_table.php
@@ -33,6 +33,7 @@ return new class extends Migration
      */
     public function down(): void
     {
+        Schema::dropIfExists('product_imagen');
         Schema::dropIfExists('product');
     }
 };
