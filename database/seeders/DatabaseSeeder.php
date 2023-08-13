c009656fa75e781844d01f2bd91df85a85b66563
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-08-13 15:16:09 -0400

    Product tables and model added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\ProductImagen;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +21,10 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        Category::factory(5)->create();
+        Product::factory(50)
+            ->has(ProductImagen::factory()->count(3))
+            ->create();
     }
 }
