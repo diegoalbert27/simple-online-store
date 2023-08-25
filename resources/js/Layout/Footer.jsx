ea3bf3b559486ec38b1669df01c64d512c6096f8
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-08-24 21:53:42 -0400

    text center footer added

--- resources/js/Layout/Footer.jsx
+++ resources/js/Layout/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="border-top p-3 w-100">
+        <footer className="border-top p-3 w-100 text-center">
             <div className="container">All rights reserved &copy; - 2023</div>
         </footer>
     );
