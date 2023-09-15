888ad723e5462ea754c0039c0fee3e3941a74dfb
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-09-14 22:52:58 -0400

    feat: detail order date added

--- resources/js/Pages/Order/Detail.jsx
+++ resources/js/Pages/Order/Detail.jsx
@@ -4,14 +4,28 @@ import { useCart } from "../../hook/useCart";
 export default function Detail({ cart }) {
     const { getTotalPriceCart } = useCart()
 
+    const getFormatDate = (formatDate) => {
+        const date = new Date(formatDate)
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDay()
+
+        const addZeroDate = (date) => (
+            date <= 10 ? `0${date}` : date
+        )
+
+        return `${addZeroDate(day)}-${addZeroDate(month)}-${year}`
+    }
+
     return (
         <div className="border p-5 rounded gap-0">
             <div className="d-flex justify-content-between mb-3">
                 <div>
+                    <p className="mb-0 text-secondary">{getFormatDate(cart.created_at)}</p>
                     <p className="mb-0 fw-bold">Nro. de Orden</p>
                     <h1 className="fs-2 mb-0 fw-light">#000{cart.id_cart}</h1>
                 </div>
-                <div>
+                <div className="mt-3">
                     <p className="fw-light mt-0 fs-4 mb-0">Total: ${getTotalPriceCart(cart.cart_product)}</p>
                     <p className="fw-light">Total de {cart.cart_product.length} productos</p>
                 </div>
