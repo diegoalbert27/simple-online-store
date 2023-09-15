73bacd87eb52854fe1fbf3a182e1f60e3351e524
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-09-14 22:58:37 -0400

    feat: count product in cart added on total

--- resources/js/hook/useCart.jsx
+++ resources/js/hook/useCart.jsx
@@ -8,7 +8,7 @@ export function useCart() {
         let totalPriceCart = 0
         products.forEach(product => {
             const PRICE = product.price_product === undefined ? 'price' : 'price_product'
-            return totalPriceCart += product[PRICE]
+            return totalPriceCart += (product[PRICE]) * product.count
         })
 
         return totalPriceCart.toFixed(2)
