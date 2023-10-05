df58123f370d4fda49315778746cc8a02d10fec9
Diego Hinagas <hinagasrodriguez@gmail.com>
2023-10-04 20:22:32 -0400

    fix: filters fixed in show

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -9,7 +9,7 @@ import { useEffect, useContext } from 'react'
 import { ProductContext } from "../context/products";
 
 export default function Welcome({ productsPage, categories, products }) {
-    const { filtersProducts, setCategories } = useFilters()
+    const { filtersProducts, setCategories, filters } = useFilters()
     const filteredProducts = filtersProducts(products)
 
     const { setProducts } = useContext(ProductContext)
@@ -19,14 +19,15 @@ export default function Welcome({ productsPage, categories, products }) {
         setProducts(products)
     }, [])
 
+    console.log(filters)
+
     return (
         <>
             <Header />
 
             <div className="mb-3">
                 {
-                    filteredProducts.length > 0 ?
-                        <Products products={filteredProducts} /> :
+                    (filters.minPrice === 0 && filters.category === 'all') && (
                         <>
                             <Products products={productsPage.data} />
 
@@ -34,6 +35,14 @@ export default function Welcome({ productsPage, categories, products }) {
                                 <Pagination links={productsPage.links} />
                             </div>
                         </>
+                    )
+                }
+
+
+                {
+                    filteredProducts.length > 0 && (
+                        <Products products={filteredProducts} />
+                    )
                 }
             </div>
         </>
